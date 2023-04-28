d54fbddaac996b7bbd1dcb979cc7cfe412b815d9
buyanaa <buyanaa@buyanaas-macbook-pro.local>
2023-04-28 13:48:08 +0800

    boom

---
+++ pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '@/components/Header'
+import Sidebar from '@/components/Sidebar'
+
+export default function Home() {
+  return (
+    <div className='w-screen h-screen'>
+        <Header />
+        <Sidebar />
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Home = () => {
+  return (
+    <>
+      <div>boom</div>
+    </>
+  )
+}
+
+export default Home
