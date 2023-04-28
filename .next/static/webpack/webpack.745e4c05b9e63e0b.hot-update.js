b409a82b372b4a9845a9c28492faf06101456403
buyanaa <buyanaa@buyanaas-macbook-pro.local>
2023-04-28 14:23:18 +0800

    test

--- .next/static/webpack/webpack.745e4c05b9e63e0b.hot-update.js
+++ .next/static/webpack/webpack.745e4c05b9e63e0b.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "d27d6753ae72477e"; }
+/******/ 	__webpack_require__.h = function() { return "f4a88d80978ff5a5"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <>
       <div>boom</div>
+      <div>baam</div>
     </>
   )
 }
