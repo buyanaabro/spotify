bbb3652ebf93bb66d654d562e473f42b47c3423b
buyanaa <buyanaa@buyanaas-macbook-pro.local>
2023-04-30 13:40:36 +0800

    added feature: change header opacity when scrolling down

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "93f6e565a788b556"; }
+/******/ 		__webpack_require__.h = function() { return "79f24303829d357d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
