75d353477418993fbe9e16ab4fc64557950aba76
buyanaa <buyanaa@buyanaas-macbook-pro.local>
2023-05-02 17:12:18 +0800

    improved the ArtistDetails page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c9d9cc65d855af6c"; }
+/******/ 		__webpack_require__.h = function() { return "8d334decd648f0fc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
