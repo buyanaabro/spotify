4bfef932b66ae639feb96fb862d1475c7d3a85a8
K1T20 <56916807+bangme4@users.noreply.github.com>
2023-05-04 17:19:04 +0800

    Almost done Home

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "08240c638df3775c"; }
+/******/ 		__webpack_require__.h = function() { return "711795177fde4ae3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,61 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <>
-      <div className="w-full h-96 bg-black flex flex-row items-center space-x-3">
-        <div className="w-52 h-80 bg-white ml-12"></div>
-        <div className="w-52 h-80 bg-white"></div>
-        <div className="w-52 h-80 bg-white"></div>
+      <div className="w-full h-96 bg-[#121212] flex flex-row items-center space-x- decoration-1">
+        <div className="w-52 h-80 bg-[#121212] ml-12 flex flex-col">
+          <div className="w-full h-12 flex items-center text-white font-medium text-md">
+            Company
+          </div>
+          <div className="w-full h-48 bg-[#121212] grid grid-rows-6">
+            <button className="w-full flex justify-start items-center text-gray-400 hover:underline hover:underline-offset-2 hover:text-white">
+              About
+            </button>
+            <button className="w-full flex justify-start items-center text-gray-400 hover:underline hover:underline-offset-2 hover:text-white">
+              Jobs
+            </button>
+            <button className="w-full flex justify-start items-center text-gray-400 hover:underline hover:underline-offset-2 hover:text-white">
+              For the Records
+            </button>
+          </div>
+        </div>
+        <div className="w-52 h-80 bg-[#121212] ml-12 flex flex-col">
+          <div className="w-full h-12 flex items-center text-white font-medium text-md">
+            Communities
+          </div>
+          <div className="w-full h-48 bg-[#121212] grid grid-rows-6">
+            <button className="w-full flex justify-start items-center text-gray-400 hover:underline hover:underline-offset-2 hover:text-white">
+              For artists
+            </button>
+            <button className="w-full flex justify-start items-center text-gray-400 hover:underline hover:underline-offset-2 hover:text-white">
+              Developers
+            </button>
+            <button className="w-full flex justify-start items-center text-gray-400 hover:underline hover:underline-offset-2 hover:text-white">
+              Advertising
+            </button>
+            <button className="w-full flex justify-start items-center text-gray-400 hover:underline hover:underline-offset-2 hover:text-white">
+              Investors
+            </button>
+            <button className="w-full flex justify-start items-center text-gray-400 hover:underline hover:underline-offset-2 hover:text-white">
+              Vendors
+            </button>
+            <button className="w-full flex justify-start items-center text-gray-400 hover:underline hover:underline-offset-2 hover:text-white">
+              Spotify for Work
+            </button>
+          </div>
+        </div>
+        <div className="w-52 h-80 bg-[#121212] ml-12 flex flex-col">
+          <div className="w-full h-12 flex items-center text-white font-medium text-md">
+            Useful links
+          </div>
+          <div className="w-full h-48 bg-[#121212] grid grid-rows-6">
+            <button className="w-full flex justify-start items-center text-gray-400 hover:underline hover:underline-offset-2 hover:text-white">
+              Support
+            </button>
+            <button className="w-full flex justify-start items-center text-gray-400 hover:underline hover:underline-offset-2 hover:text-white">
+              Free Mobile App
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
