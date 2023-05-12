52960cc55cbb8a91390af03c90cf5a91ac74fa12
buyanaa <buyanaa@buyanaas-macbook-pro.local>
2023-05-12 15:47:47 +0800

    boom

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "612c4c586187a0c7"; }
+/******/ 		__webpack_require__.h = function() { return "12151f43c50bbf13"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/Home.js
+++ pages/Home.js
@@ -1,10 +1,10 @@
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import Footer from "@/components/Footer";
-import Footer2 from "./Footer2";
+import Footer2 from "../components/Footer2";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import Card from "@/components/Artists";
+import Card from "@/components/Card";
 import { data } from "./data";
 
 const Artists = () => {
