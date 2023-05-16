2e6ea37493f643f960f4a497c0be00f9076ec798
buyanaa <buyanaa@buyanaas-macbook-pro.local>
2023-05-16 08:08:31 +0800

    done artist page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6b47bfadc4525764"; }
+/******/ 		__webpack_require__.h = function() { return "826b836d77a695ff"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
