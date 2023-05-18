a05907b9278920ce719af25ed5b236fb3f23c869
K1T20 <56916807+bangme4@users.noreply.github.com>
2023-05-18 14:20:31 +0800

    Iredui

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e75f4ad485fef5ef"; }
+/******/ 		__webpack_require__.h = function() { return "83f70c8faf7297c0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/Playlist/[id].js
+++ pages/Playlist/[id].js
@@ -2,6 +2,8 @@ import React from "react";
 import { playList } from "../Playlistdata";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
+import Footer from "@/components/Footer";
+import Footer2 from "@/components/Footer2";
 
 // export async function getServerSideProps(context) {
 //   const songs = playList.find((e) => e.number == context.query.id);
@@ -11,23 +13,45 @@ import Sidebar from "@/components/Sidebar";
 // }
 export default function Playlist() {
   return (
-    <div className="w-screen h-screen flex flex-row bg-[#121212]">
-      <div className="w-72 h-full">
-        <Header />
-        <Sidebar />
-      </div>
-      <div className="w-full h-full rounded-2xl">
-        <div className="w-full h-80 rounded-t-2xl bg-white flex items-center px-8 bg-gradient-to-b from-stone-600 to-black/70 ">
-          <div className="w-52 h-52 bg-stone-800 flex items-center justify-center text-white/40 shadow group">
-            <MusicIcon />
-            <div className="absolute w-52 h-52 bg-stone-800 hidden group-hover:flex flex-col justify-center items-center">
-              <EditIcon />
-              <p>Choose Photo</p>
+    <>
+      <div className="w-screen h-screen flex flex-row bg-[#121212]">
+        <div className="w-72 h-full">
+          <Header />
+          <Sidebar />
+        </div>
+        <div className="w-full h-full rounded-2xl flex items-center flex-col bg-gradient-to-b from-stone-900 to-[#121212]">
+          <div className="w-full h-80 rounded-t-2xl bg-white flex items-center px-8 bg-gradient-to-b from-stone-600 to-black/80">
+            <div className="w-52 h-52 bg-stone-800 flex items-center justify-center drop-shadow-2xl text-white/40  group mt-10">
+              <MusicIcon />
+              <button className="absolute w-52 h-52 bg-stone-800 hidden drop-shadow-2xl group-hover:flex flex-col justify-center  items-center">
+                <EditIcon />
+                <p>Choose Photo</p>
+              </button>
             </div>
+            <div className="w-fit h-44 mt-16 ml-10 flex flex-col text-white">
+              <div className="text-sm">Playlist</div>
+              <div className="text-8xl font-bold mt-3">My Playlist#</div>
+              <div className="w-fit h-full text-sm flex items-end">Profile</div>
+            </div>
+          </div>
+          <div className="w-11/12 h-28 border-b border-gray-600">
+            <More />
+          </div>
+          <div className="w-11/12 h-fit mt-4">
+            <span className="text-white text-2xl font-semibold">
+              Let's find something for your playlist
+            </span>
           </div>
         </div>
       </div>
-    </div>
+      <div className="w-screen h-fit flex flex-row bg-[#121212]">
+        <div className="w-72 h-full"></div>
+        <div className="w-full">
+          <Footer />
+          <Footer2 />
+        </div>
+      </div>
+    </>
   );
 }
 
@@ -64,6 +88,24 @@ const EditIcon = () => {
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+      />
+    </svg>
+  );
+};
+const More = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-9 h-9 text-gray-500 mt-8"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
       />
     </svg>
   );
